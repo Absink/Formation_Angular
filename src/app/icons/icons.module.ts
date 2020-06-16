d08291d48d581755b8719aac4d3b868f58b1f58d
Olivier Blaivie <o.blaivie@coperbee.fr>
2020-06-16 12:36:51 +0200

    Creation des icones

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IconNavComponent, IconEditComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [
+    FontAwesomeModule
   ]
 })
 export class IconsModule { }
