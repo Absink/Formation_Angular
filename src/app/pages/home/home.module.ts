b67bca2bd96650bed428c085e2fe9f94c73474e2
Olivier Blaivie <o.blaivie@coperbee.fr>
2020-06-17 09:19:26 +0200

    Mise en place du routage (Fix)

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageHomeComponent } from './components/page-home/page-home.component';
+
+
+
+@NgModule({
+  declarations: [PageHomeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
