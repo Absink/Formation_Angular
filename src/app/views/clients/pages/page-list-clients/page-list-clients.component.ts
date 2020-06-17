9d47f6d505bf0e383eaf0d4c92d026848fe63bd4
Olivier Blaivie <o.blaivie@coperbee.fr>
2020-06-17 11:10:08 +0200

    Exemple Observable

--- src/app/views/clients/pages/page-list-clients/page-list-clients.component.ts
+++ src/app/views/clients/pages/page-list-clients/page-list-clients.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-page-list-clients',
@@ -10,6 +11,22 @@ export class PageListClientsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // 1. Creation Observable
+    const datas = new Observable(obs => {
+      obs.next('M2i');      // 'next' envoie une valeur;
+      obs.next('Airbus');
+      obs.complete();       // 'complete' signale la fin
+    });
+
+    // 2. Souscription
+    datas.subscribe({
+      next: value => console.log(value),
+      complete: () => console.log('End')
+    })
+
+    // 3. Demo avec F12 !
+
+    // 4. Effacer le code
   }
 
 }
