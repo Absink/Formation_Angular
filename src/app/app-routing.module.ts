6563c0d891f69f0bd9dbfe635f0c400477137c69
Olivier Blaivie <o.blaivie@coperbee.fr>
2020-06-17 10:49:07 +0200

    Ajout du LazyLoading pour le routage (Fix)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, Router } from '@angular/router';
 import { PageNotFoundComponent } from './views/page-not-found/pages/page-not-found/page-not-found.component';
 import { PageHomeComponent } from './views/home/pages/page-home/page-home.component';
 
@@ -8,14 +8,21 @@ import { PageHomeComponent } from './views/home/pages/page-home/page-home.compon
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: PageHomeComponent },
-  // { path: 'orders', component: PageListOrdersComponent },
   { path: 'orders', loadChildren: () => import('./views/orders/orders.module').then(m => m.OrdersModule) },
   { path: 'clients', loadChildren: () => import('./views/clients/clients.module').then(m => m.ClientsModule) },
   { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false,   // true et F12 pour l'exemple
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(router: Router) {
+    const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 1));
+  }
+}
