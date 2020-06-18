d4658475f936310c6c3a15157797cfb114323f2c
Olivier Blaivie <o.blaivie@coperbee.fr>
2020-06-18 12:18:57 +0200

    Template de tableau Dark

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { TableLightComponent } from './components/table-light/table-light.component';
 import { TotalPipe } from './pipes/total.pipe';
 import { ColorStateDirective } from './directives/color-state.directive';
+import { TableDarkComponent } from './components/table-dark/table-dark.component';
 
 
 
 @NgModule({
-  declarations: [TableLightComponent, TotalPipe, ColorStateDirective],
+  declarations: [TableLightComponent, TotalPipe, ColorStateDirective, TableDarkComponent],
   imports: [
     CommonModule
   ], exports: [
     TableLightComponent,
+    TableDarkComponent,
     TotalPipe,
     ColorStateDirective
   ]
