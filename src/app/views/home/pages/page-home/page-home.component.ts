7047f5302eebc51249debc0eeb8d277d998bea0a
Absink <35706202+Absink@users.noreply.github.com>
2020-11-24 11:46:48 +0100

    Update page-home.component.ts

--- src/app/views/home/pages/page-home/page-home.component.ts
+++ src/app/views/home/pages/page-home/page-home.component.ts
@@ -10,6 +10,25 @@ export class PageHomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // 1. Creation Observable (parler du mot clé CONST)
+    const data = new Observable(observer => {
+      observer.next(1);     // 'next' envoie une valeur;
+      observer.next(5);
+      // 4. Ajouter l'error:  observer.error(new Error('Error detected')):
+      // 5. Retester le console log, l'erreur inréromp la fin du traitement
+      observer.next(30);
+      observer.complete();  // 'complete' signale la fin
+    });
+
+    // 2. Souscription (3 états)
+    data.subscribe({
+      next: value => console.log(value),
+      error: err => console.error(err),
+      complete: () => console.log('Finish')
+    });
+    // 'error' et 'complete' = 'reject' et 'resolve' pour les promises
+
+    // 3. Demo avec F12 !
   }
 
 }
